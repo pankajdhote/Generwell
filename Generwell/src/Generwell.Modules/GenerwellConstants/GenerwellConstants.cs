98c89ec24baeffa48aaffc2c349f77a0dcf1e2c4
Pankaj Dhote <pankaj.dhote@anarsolutions.com>
2016-11-16 11:47:20 +0530

    added by pd

--- Generwell/src/Generwell.Modules/GenerwellConstants/GenerwellConstants.cs
+++ Generwell/src/Generwell.Modules/GenerwellConstants/GenerwellConstants.cs
@@ -19,13 +19,10 @@ namespace Generwell.Modules.GenerwellConstants
             public const string Token = "api/Token";
             public const string Support = "https://anar.whelby.com/api/v2016.1/support/contact";
             public const string Well = "https://anar.whelby.com/api/v2016.1/Wells";
-<<<<<<< HEAD
             public const string Task = "https://anar.whelby.com/api/v2016.1/Tasks";
-=======
             public const string WellFilter = "https://anar.whelby.com/api/v2016.1/wells?filterId";
             public const string WellLineReports = "https://anar.whelby.com/api/v2016.1/linereports?fieldLevelId=1";
             public const string WellDetails = "https://anar.whelby.com/api/v2016.1/wells/7361/linereports/5006";
->>>>>>> 457905fc65a8a15167d794b68fc72b22c53c1c8b
             public const string Filters = "https://anar.whelby.com/api/v2016.1/filters";
             public const string Status = "Active";
           
@@ -37,10 +34,6 @@ namespace Generwell.Modules.GenerwellConstants
             public static string WellId = string.Empty;
 
             public static string TimeZone = "EDT";
-
-            
-
-
 
         }
     }
